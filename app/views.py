1359035e4cdaefb722900c0ae0faffb3c696ba3c
Deandrew007 <deamoore07@gmail.com>
2021-03-12 11:43:58 -0500

    added vue setup and base html files

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app 
 from app import models
 from app import forms
+from flask import render_template, request, redirect, url_for, flash
+
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="8080")
