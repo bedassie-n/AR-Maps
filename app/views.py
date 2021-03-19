9701d48c32aa5827271738f6ce85adecb7a4dfe5
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-03-19 12:23:45 -0500

    started building

--- app/views.py
+++ app/views.py
@@ -9,5 +9,10 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/nav')
+def ar():
+    """Render camera with ar experience  <19/3/2021 N.Bedassie>"""
+    return render_template("map.html")
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
