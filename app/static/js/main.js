b0527399f7421cf9e4ad79d8e372931dfa42f62e
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-03-22 15:36:53 -0500

    testing ar.js

---
+++ app/static/js/main.js
@@ -0,0 +1,14 @@
+window.onload = {
+    var x = document.getElementById("current_location");
+    function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    }
+    function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+    }
+}
